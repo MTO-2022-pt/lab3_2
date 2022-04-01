c7011c6ed46a5da84b21e093765a4eef2b2c06f2
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-04-01 18:34:07 +0200

    IDE setup

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,7 +1,5 @@
 package edu.iis.mto.time;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +7,13 @@ import org.junit.jupiter.api.Test;
 class OrderTest {
 
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() {
+
+    }
 
     @Test
     void test() {
-        fail("Not yet implemented");
+        
     }
 
 }
