0a15569b7b3a5cd85f8347929459839989c522b1
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-04-07 22:35:28 +0200

    testy dla czasu poniżej 24 godzin

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -11,16 +12,60 @@ import java.time.Month;
 
 class OrderTest {
 
+    Order order;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp(){
+    }
 
     @Test
-    void test() {
-        Order order = new Order(LocalDateTime.of(2023, Month.APRIL, 29, 23, 12, 12));
-        System.out.println(order.getOrderState());
+    void OneSecondFromNowOneOrderItemStateTest() {
+        Order order = new Order(LocalDateTime.now().plusSeconds(1));
+        order.addItem(new OrderItem());
+        assertEquals(order.getOrderState(), Order.State.CREATED);
+    }
+
+    @Test
+    void tenMinutesFromNowOneOrderItemStateTest() {
+        Order order = new Order(LocalDateTime.now().plusMinutes(10));
         order.addItem(new OrderItem());
         order.submit();
-        order.confirm();
+        assertEquals(order.getOrderState(), Order.State.SUBMITTED);
     }
+
+    @Test
+    void sevenHoursFromNowFiveOrderItemsStateTest() {
+        Order order = new Order(LocalDateTime.now().plusHours(7));
+        for(int i = 0; i< 5; i++){
+            order.addItem(new OrderItem());
+        }
+        order.submit();
+        order.confirm();
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+    }
+
+    @Test
+    void moreThanTwelveHoursFromNowFiveOrderItemsStateTest() {
+        Order order = new Order(LocalDateTime.now().plusHours(14));
+        for(int i = 0; i< 5; i++){
+            order.addItem(new OrderItem());
+        }
+        order.submit();
+        order.confirm();
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+    }
+
+    @Test
+    void OneDayFromNowFiveOrderItemsStateTest() { //almost a day passed
+        Order order = new Order(LocalDateTime.now().plusDays(1));
+        for(int i = 0; i< 5; i++){
+            order.addItem(new OrderItem());
+        }
+        order.submit();
+        order.confirm();
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
+    }
+
+
 
 }
