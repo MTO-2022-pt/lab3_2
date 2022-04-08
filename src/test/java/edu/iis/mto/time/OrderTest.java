03e3a750a6b7db7f5e84ae30176e49ea2ab5cf55
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-04-08 15:03:13 +0200

    fixed orderExpiredThrowsOrderExpiredException to check for proper exception added orderExpiredExpectingCancelledState test

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.time;
 
+import static edu.iis.mto.time.Order.State.CANCELLED;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -13,13 +14,24 @@ class OrderTest {
     private Order expiredOrderInstance;
     @BeforeEach
     void setUp() throws Exception {
-        expired = LocalDateTime.now().plusDays(1);
+        expired = LocalDateTime.now().plusHours(25).plusMinutes(1);
         expiredOrderInstance = new Order(expired);
     }
 
     @Test
     void orderExpiredThrowsOrderExpiredException() {
-        assertThrowsExactly(OrderStateException.class,()->expiredOrderInstance.confirm());
+        expiredOrderInstance.submit();
+        assertThrowsExactly(OrderExpiredException.class,()->expiredOrderInstance.confirm());
+    }
+
+    @Test
+    void orderExpiredExpectingCancelledState() {
+        try{
+            expiredOrderInstance.submit();
+            expiredOrderInstance.confirm();
+        }catch(OrderExpiredException e){
+            assertEquals(CANCELLED,expiredOrderInstance.getOrderState());
+        }
     }
 
 }
