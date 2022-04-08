d071aab0d0f73f1e03e3de66d14a61c875a915de
ImBioMario <m.sukiennik95@gmail.com>
2022-04-08 21:02:50 +0200

    Code cleanup.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,31 +1,13 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
-import net.bytebuddy.asm.Advice;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import org.mockito.internal.matchers.Or;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.*;
-import java.time.temporal.ChronoUnit;
 
-@ExtendWith(MockitoExtension.class)
 class OrderTest {
-
     Order testOrder;
-
-    @BeforeEach
-    void setUp(){
-
-    }
 
     @Test
     void expiredEmptyOrderThrows() {
