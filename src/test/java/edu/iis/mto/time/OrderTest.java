203b90e1be54c26cf103d5852597112151664461
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-04-08 14:46:08 +0200

    testing if OrderExpiredException is thrown

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -5,15 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 
 class OrderTest {
-
+    private LocalDateTime expired;
+    private Order expiredOrderInstance;
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        expired = LocalDateTime.now().plusDays(1);
+        expiredOrderInstance = new Order(expired);
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void orderExpiredThrowsOrderExpiredException() {
+        assertThrowsExactly(OrderStateException.class,()->expiredOrderInstance.confirm());
     }
 
 }
