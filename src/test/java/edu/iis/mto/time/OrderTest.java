54e08010f320538da7aa5d7ef1c156f79d12b996
gontarek.jan <228737@edu.p.lodz.pl>
2022-04-08 18:16:48 +0200

    added tests

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -8,12 +8,39 @@ import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
+    private FakeClock fakeClock;
+
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+        fakeClock = new FakeClock();
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void shouldThrowOrderExpiredException() {
+        long nextDaySec = 48 * 60 * 60;
+        fakeClock.nextInstant(nextDaySec);
+        Order order = new Order(fakeClock);
+
+        order.addItem(new OrderItem());
+        order.submit();
+        
+        assertThrows(OrderExpiredException.class, () -> {
+            order.confirm();
+        });
+    }
+
+    @Test
+    void shouldNotThrowOrderExpiredException() {
+        long nextDaySec = 10 * 60 * 60;
+        fakeClock.nextInstant(nextDaySec);
+        Order order = new Order(fakeClock);
+
+        order.addItem(new OrderItem());
+        order.submit();
+        
+        assertDoesNotThrow(() -> {
+            order.confirm();
+        });
     }
 
 }
