48b72e8068bdb29ddfe6cb72d8f827833b06c1e1
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-05-20 13:16:33 +0200

    dodalem test do sprawdzania dla tego samego czasu

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,19 +1,42 @@
 package edu.iis.mto.time;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-
+@ExtendWith(MockitoExtension.class)
 class OrderTest {
+    @Mock
+    private Clock clock;
+    private Order order;
+    private final Instant date = Instant.parse("2005-01-01T10:00:00Z");
 
     @BeforeEach
-    void setUp() throws Exception {}
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
+    void setUp() {
+        when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+        order = new Order(clock);
     }
 
+    @Test
+    void sameTime() {
+        Instant expirationTime = date.plus(0, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(date).thenReturn(expirationTime);
+        try {
+            order.submit();
+            order.confirm();
+            assertSame(Order.State.CONFIRMED, order.getOrderState());
+        } catch (OrderExpiredException e) {
+            fail(e.getMessage());
+        }
+    }
 }
