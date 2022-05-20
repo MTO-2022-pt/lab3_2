ef95100dbb21dd4f2021bb2938c06551b40885ab
karwowski.franciszek <228765@edu.p.lodz.pl>
2022-05-20 13:25:59 +0200

    dodalem testy sprawdzajace uplyniecie godziny i wygasniecie

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,7 +29,7 @@ class OrderTest {
     }
 
     @Test
-    void sameTime() {
+    void sameTimeExpectedOrderConfirmed() {
         Instant expirationTime = date.plus(0, ChronoUnit.HOURS);
         when(clock.instant()).thenReturn(date).thenReturn(expirationTime);
         try {
@@ -38,5 +39,32 @@ class OrderTest {
         } catch (OrderExpiredException e) {
             fail(e.getMessage());
         }
+    }
+
+    @Test
+    void differentTimeExpectedOrderCancelled() {
+        Instant expirationTime = date.plus(9999999, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(date).thenReturn(expirationTime);
+        
+        order.addItem(new OrderItem());
+        order.submit();
+        Assertions.assertThrows(OrderExpiredException.class, () -> order.confirm());
+        
+        assertEquals(order.getOrderState(), Order.State.CANCELLED);
+    }
+
+    @Test
+    void differentTimeExpectedOrderConfirmed() {
+        Instant expirationTime = date.plus(1, ChronoUnit.HOURS);
+        when(clock.instant()).thenReturn(date).thenReturn(expirationTime);
+        try {
+            order.submit();
+            order.confirm();
+            assertSame(Order.State.CONFIRMED, order.getOrderState());
+        } catch (OrderExpiredException e) {
+            fail(e.getMessage());
+        }
+        
+        assertEquals(order.getOrderState(), Order.State.CONFIRMED);
     }
 }
